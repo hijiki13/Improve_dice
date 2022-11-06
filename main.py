24a131e7a9790da8f58b974ab81313ef2e192e19
Sia <anastasia130499@gmail.com>
2022-11-06 20:25:23 +0200

    Added bet verification

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ def bet_play():
     score = 500
     print(roll)
     while score > 0:
-        bet_p = input('Your bet (points): ')
-        bet_n = input('Your bet on (number): ')
+        bet_p = input('Your bet is (points): ')
+        bet_n = input('You bet on (number): ')
+
         if bet_p.isdigit() and bet_n.isdigit():
-            bet_p = int(bet_p)
-            bet_n = int(bet_n)
-            roll = input('Roll it! (Enter)\n')        
-            if roll:
-                print('Goodbye!')
-                break
+            bet_p, bet_n = int(bet_p), int(bet_n)
+            
+            if bet_p > score:
+                print("Can't bet more points then you have.")
+                continue
+            elif bet_n not in range(1, 7):
+                print('Invalid bet (not in range 1-6)')
+                continue       
+            
             num = randint(1,6)
             if num == bet_n:
                 score += 3*bet_p
@@ -21,8 +25,14 @@ def bet_play():
             else:
                 score -= bet_p
                 print(f'You got {num}! Try again!\nScore: {score}')
+
+            roll = input('Go again? (Enter - continue; Q - exit)\n')        
+            if roll:
+                print('Goodbye!')
+                break
+            
         else:
             print('Can be only numbers!')
     else:
         print('Sorry, you have no more points!')
-# bet_play()
+bet_play()
