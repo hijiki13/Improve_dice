80f58b871432b53c508477123bebcec3bb6ae93e
Sia <anastasia130499@gmail.com>
2022-11-07 22:25:01 +0200

    Added logging/several exceptions handling

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from random import randint
 from time import sleep
+from datetime import datetime
+# Кубик
+class Log():
+    def __init__(self, log_type, log_msg):
+        self.log_type = log_type
+        self.log_msg = log_msg
+        self.time = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    
+    def __str__(self):
+        return f'{self.time} - {self.log_type} - {self.log_msg}\n'
 
 def str_center(*strings):
     for i in strings:
         print(i.center(55))
     print()
 
-# Кубик
-def bet_play():
+def bet_play(logs):
     dice = {
         1: (
             '┌─────────┐',
@@ -56,40 +65,83 @@ def bet_play():
     score = 500
     str_center("Let's play!\U0001F3B2", 'You have 500 points!')
     
-    while True:
-        bet_n = input('You bet on (number): ')
-        bet_p = input('Your bet is (points): ')
-        print()
+    try:
+        while True:
+            bet_n = input('You bet on (number): ')
+            bet_p = input('Your bet is (points): ')
+            print()
 
-        if bet_p.isdigit() and bet_n.isdigit():
-            bet_p, bet_n = int(bet_p), int(bet_n)
+            if bet_p.isdigit() and bet_n.isdigit():
+                bet_p, bet_n = int(bet_p), int(bet_n)
             
-            if bet_p > score:
-                str_center("Can't bet more points then you have.")
-                continue
-            elif bet_n not in range(1, 7):
-                str_center('Invalid bet (not in range 1-6)')
-                continue       
+                if bet_n not in range(1, 7):
+                    str_center('Invalid bet (not in range 1-6)')
+
+                    log_entry = Log('Error', 'Invalid bet number')
+                    logs.write(str(log_entry))
+                    continue 
+                elif bet_p > score:
+                    str_center("Can't bet more points then you have.")
+
+                    log_entry = Log('Error', 'Invalid bet size')
+                    logs.write(str(log_entry))
+                    continue    
             
-            num = randint(1,6)
-            sleep(.5)
-            str_center(*dice[num])
-            if num == bet_n:
-                score += 3*bet_p
-                str_center(f'You got {num}! You win!', f'~~~~~~~SCORE~~~~~~~', f'{score}')
-            else:
-                score -= bet_p
-                str_center(f'You got {num}! Try again!', '~~~~~~~SCORE~~~~~~~', f'{score}')
-            
-            if score < 1:
-                str_center('Sorry, you have no more points!')
-                return
+                num = randint(1,6)
+                sleep(.5)
+                str_center(*dice[num])
+                if num == bet_n:
+                    score += 3*bet_p
+                    str_center(f'You got {num}! You win!', f'~~~~~~~SCORE~~~~~~~', f'{score}')
+                    log_entry = Log('Info', f'WIN with bet on {bet_n} - Number: {num}')
+                    logs.write(str(log_entry))
+                else:
+                    score -= bet_p
+                    str_center(f'You got {num}! Try again!', '~~~~~~~SCORE~~~~~~~', f'{score}')
+                    log_entry = Log('Info', f'LOST with bet on {bet_n} - Number: {num}')
+                    logs.write(str(log_entry))
+                if score < 1:
+                    str_center('Sorry, you have no more points!')
+                    log_entry = Log('Event', 'Program ended (no points)')
+                    logs.write(str(log_entry))
+                    return
         
-            str_center('Go again?', '(Enter - continue; Q - exit)')
-            roll = input()        
-            if roll:
-                str_center('Goodbye!')
-                return   
-        else:
-            str_center('Can be only numbers!')
-bet_play()
+                str_center('Go again?', '(Enter - continue; Q - exit)')
+                roll = input()        
+                if roll:
+                    str_center('~~~~~~GOODBYE!~~~~~~')
+
+                    log_entry = Log("Event", "Program ended (User's choice)")
+                    logs.write(str(log_entry))
+                    return   
+            else:
+                str_center('Can be only numbers!')
+
+                log_entry = Log('Error', 'Invalid input')
+                logs.write(str(log_entry))
+    except KeyboardInterrupt:
+        log_entry = Log('Error', 'KeyboardInterrupt')
+        logs.write(str(log_entry))
+        log_entry = Log('Event', 'Program ends (Error)')
+        logs.write(str(log_entry))
+    except Exception as err:
+        log_entry = Log('Error', f'{type(err).__name__}')
+        logs.write(str(log_entry))
+        log_entry = Log('Event', 'Program ends (Error)')
+        logs.write(str(log_entry))
+
+def logs():
+    with open('logs.txt', 'a') as log:
+        log_entry = Log('Event', 'Program starts')
+        log.write(str(log_entry))
+        bet_play(log)
+        log.write('\n')
+
+def read_logs():
+    pass
+        
+def main():
+    logs()
+
+if __name__ == '__main__':
+    main()
